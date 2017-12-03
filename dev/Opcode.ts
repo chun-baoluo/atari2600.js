7b40503fda1bd35c6630e17143b2dc6899b9bfa2
Pavel Bakharev <baharevpavel@gmail.com>
2017-12-03 19:12:55 +0700

    Couple additional opcodes and tests for them

--- dev/Opcode.ts
+++ dev/Opcode.ts
@@ -5,56 +5,62 @@ import { Convert } from './Common';
 // TODO: Memory mirroring
 
 export class Opcode {
-    
+
     private static isNextPage(pc1: number, pc2: number) {
-        return ('000' + pc1.toString(16)).slice(-4).charAt(0) != ('000' + pc2.toString(16)).slice(-4).charAt(0);    
+        return ('000' + pc1.toString(16)).slice(-4).charAt(0) != ('000' + pc2.toString(16)).slice(-4).charAt(0);
     };
-    
+
     // BPL nnn
     public static 0x10() {
         //Flag.N = 1;
-                
+
         if(Flag.N == 1) {
 			Register.PC++;
 			return 2;
 		};
-        
+
 		let num: number = Convert.toInt8(Rom.data[++Register.PC]);
-        
-        return 3 + (this.isNextPage(Register.PC, Register.PC += num) ? 1 : 0);        
+
+        return 3 + (this.isNextPage(Register.PC, Register.PC += num) ? 1 : 0);
     };
-    
+
     // JMP nnnn
     public static 0x4C() {
         let low: number = Rom.data[++Register.PC];
         let high: number = Rom.data[++Register.PC];
         let address: number = ((high & 0xFF) << 8) | (low & 0xFF);
-    
+
         Register.PC = address & 0xFF;
         Register.PC--;
-    
+
         return 3;
     };
 
     // SEI
     public static 0x78() {
         Flag.I = 1;
-        
+
         return 2;
     };
-    
+
+    // STY nn
+    public static 0x84() {
+        RAM.write(Rom.data[++Register.PC], Register.Y);
+        return 3;
+    };
+
     // STA nn
     public static 0x85() {
         RAM.write(Rom.data[++Register.PC], Register.A);
         return 3;
     };
-    
+
     // STX nn
     public static 0x86() {
         RAM.write(Rom.data[++Register.PC], Register.X);
         return 3;
     };
-    
+
     // DEY
     public static 0x88() {
         Register.Y = Convert.toUint8(Register.Y - 1);
@@ -64,37 +70,37 @@ export class Opcode {
         } else {
             Flag.Z = 0;
         };
-        
+
         if(Convert.toBin(Register.Y).charAt(0) == '1') {
             Flag.N = 1;
         } else {
             Flag.N = 0;
         };
-        
+
         return 2;
     };
-    
-    // STA nnnn 
+
+    // STA nnnn
     public static 0x8D() {
         let low: number = Rom.data[++Register.PC];
         let high: number = Rom.data[++Register.PC];
         let address: number = ((high & 0xff) << 8) | (low & 0xff);
-        
+
         RAM.write(address, Register.A);
-        
+
         return 4;
     };
-    
+
     // STA nn, X
-    public static 0x95() {    
+    public static 0x95() {
         RAM.write(Rom.data[++Register.PC] + Register.X, Register.A);
         return 4;
     };
-    
+
     // TXS
     public static 0x9A() {
         Register.S = Register.X;
-        
+
         return 2;
     };
 
@@ -107,7 +113,7 @@ export class Opcode {
         } else {
             Flag.Z = 0;
         };
-        
+
         if(Convert.toBin(Register.Y).charAt(0) == '1') {
             Flag.N = 1;
         } else {
@@ -126,7 +132,7 @@ export class Opcode {
         } else {
             Flag.Z = 0;
         };
-        
+
         if(Convert.toBin(Register.X).charAt(0) == '1') {
             Flag.N = 1;
         } else {
@@ -135,7 +141,7 @@ export class Opcode {
 
         return 2;
     };
-    
+
     // LDA #nn
     public static 0xA9() {
         Register.A = Rom.data[++Register.PC];
@@ -145,39 +151,39 @@ export class Opcode {
         } else {
             Flag.Z = 0;
         };
-        
+
         if(Convert.toBin(Register.A).charAt(0) == '1') {
             Flag.N = 1;
         } else {
             Flag.N = 0;
         };
 
-        return 2;        
+        return 2;
     };
-    
+
     // LDA nnnn
     public static 0xAD() {
         let low: number = Rom.data[++Register.PC];
         let high: number = Rom.data[++Register.PC];
         let address: number = ((high & 0xFF) << 8) | (low & 0xFF);
-        
+
         Register.A = RAM.read(address);
-        
+
         if(Register.A == 0) {
             Flag.Z = 1;
         } else {
             Flag.Z = 0;
         };
-        
+
         if(Convert.toBin(Register.A).charAt(0) == '1') {
             Flag.N = 1;
         } else {
             Flag.N = 0;
         };
-        
+
         return 4;
     };
-    
+
     // DEX
     public static 0xCA() {
         Register.X = Convert.toUint8(Register.X - 1);
@@ -187,35 +193,35 @@ export class Opcode {
         } else {
             Flag.Z = 0;
         };
-        
+
         if(Convert.toBin(Register.X).charAt(0) == '1') {
             Flag.N = 1;
         } else {
             Flag.N = 0;
         };
-        
+
         return 2;
     };
-    
+
     // BNE
     public static 0xD0() {
         if(Flag.Z == 1) {
 			Register.PC++;
 			return 2;
 		};
-        
+
 		let num: number = Convert.toInt8(Rom.data[++Register.PC]);
-        
+
         return 3 + (this.isNextPage(Register.PC, Register.PC += num)? 1 : 0);
     };
-    
+
     // CLD
     public static 0xD8() {
         Flag.D = 0;
-        
+
         return 2;
     };
-    
+
     // INX
     public static 0xE8() {
         Register.X = Convert.toUint8(Register.X + 1);
@@ -225,13 +231,18 @@ export class Opcode {
         } else {
             Flag.Z = 0;
         };
-        
+
         if(Convert.toBin(Register.X).charAt(0) == '1') {
             Flag.N = 1;
         } else {
             Flag.N = 0;
         };
-        
+
+        return 2;
+    };
+
+    // No operator
+    public static 0xEA() {
         return 2;
     };
 };
--- test/opcodes.spec.ts
+++ test/opcodes.spec.ts
@@ -18,18 +18,18 @@ describe("CPU Jump and Control Instructions", () => {
         Register.Y = 0;
         Rom.data = new Uint8Array([]);
     });
-    
+
     it("(0x10) should jump if Negative flag is set", () => {
         Rom.data = new Uint8Array([0xD0, -0x01, 0x02]);
         Opcode[0x10]();
         chai.assert.strictEqual(Register.PC, 0);
-        
+
         Flag.N = 1;
         Register.PC = 0;
         Opcode[0x10]();
         chai.assert.strictEqual(Register.PC, 1);
     });
-    
+
     it("(0x4C) should jump to nnnn", () => {
         Rom.data = new Uint8Array([0x4C, 0x01, 0xFF]);
         Opcode[0x4C]();
@@ -46,17 +46,21 @@ describe("CPU Jump and Control Instructions", () => {
         Rom.data = new Uint8Array([0xD0, -0x01, 0x02]);
         Opcode[0xD0]();
         chai.assert.strictEqual(Register.PC, 0);
-        
+
         Flag.Z = 1;
         Register.PC = 0;
         Opcode[0xD0]();
         chai.assert.strictEqual(Register.PC, 1);
     });
-    
+
     it("(0xD8) should clear decimal mode", () => {
         Flag.D = 1;
         Opcode[0xD8]();
         chai.assert.strictEqual(Flag.D, 0);
+    });
+
+    it("(0xEA) should do nothing", () => {
+        Opcode[0xEA]();
     });
 });
 
@@ -74,43 +78,52 @@ describe("CPU Memory and Register Transfers", () => {
         Rom.data = new Uint8Array([]);
     });
 
-    it("(0x85) should set an address value (nn) to be equal regsiter A", () => {
+    it("(0x84) should set an address value (nn) to be equal register Y", () => {
+        RAM.set(0x32, 2);
+        Rom.data = new Uint8Array([0x84, 0x32]);
+        Register.Y = 5;
+
+        Opcode[0x84]();
+        chai.assert.strictEqual(RAM.get(0x32), Register.Y);
+    });
+
+    it("(0x85) should set an address value (nn) to be equal register A", () => {
         RAM.set(0x32, 2);
         Rom.data = new Uint8Array([0x85, 0x32]);
         Register.A = 5;
-        
+
         Opcode[0x85]();
         chai.assert.strictEqual(RAM.get(0x32), Register.A);
     });
-    
-    it("(0x86) should set an address value (nn) to be equal regsiter X", () => {
+
+    it("(0x86) should set an address value (nn) to be equal register X", () => {
         RAM.set(0x32, 0);
         Rom.data = new Uint8Array([0x86, 0x32]);
         Register.X = 5;
-        
+
         Opcode[0x86]();
         chai.assert.strictEqual(RAM.get(0x32), Register.X);
     });
-    
-    it("(0x8D) should set an address value (nnnn) to be equal regsiter A", () => {
+
+    it("(0x8D) should set an address value (nnnn) to be equal register A", () => {
         RAM.set(0x01, 0);
         Rom.data = new Uint8Array([0x85, 0x01, 0x00]);
         Register.A = 5;
-        
+
         Opcode[0x95]();
         chai.assert.strictEqual(RAM.get(0x01), Register.A);
     });
-    
-    it("(0x95) should set an address value + X register to be equal regsiter A", () => {
+
+    it("(0x95) should set an address value + X register to be equal register A", () => {
         RAM.set(0x32, 0);
         Rom.data = new Uint8Array([0x95, 0x31]);
         Register.X = 0x01;
         Register.A = 5;
-        
+
         Opcode[0x95]();
         chai.assert.strictEqual(RAM.get(0x32), Register.A);
     });
-    
+
     it("(0x9A) should set register S to be equal register X", () => {
         Register.X = 0xAA;
         Opcode[0x9A]();
@@ -120,52 +133,52 @@ describe("CPU Memory and Register Transfers", () => {
     it("(0xA0) should set register Y to nn, change N and Z flags", () => {
         Rom.data = new Uint8Array([0xA0, 0xFF, 0]);
         Flag.Z = 1;
-        
+
         Opcode[0xA0]();
         chai.assert.strictEqual(Register.Y, Rom.data[1]);
         chai.assert.strictEqual(Flag.N, 1);
         chai.assert.strictEqual(Flag.Z, 0);
-        
+
         Opcode[0xA0]();
         chai.assert.strictEqual(Register.Y, Rom.data[2]);
         chai.assert.strictEqual(Flag.N, 0);
         chai.assert.strictEqual(Flag.Z, 1);
     });
-    
+
     it("(0xA2) should set register X to nn, change N and Z flags", () => {
         Rom.data = new Uint8Array([0xA2, 0xFF, 0]);
         Flag.Z = 1;
-        
+
         Opcode[0xA2]();
         chai.assert.strictEqual(Register.X, Rom.data[1]);
         chai.assert.strictEqual(Flag.N, 1);
         chai.assert.strictEqual(Flag.Z, 0);
-        
+
         Opcode[0xA2]();
         chai.assert.strictEqual(Register.X, Rom.data[2]);
         chai.assert.strictEqual(Flag.N, 0);
         chai.assert.strictEqual(Flag.Z, 1);
     });
-    
+
     it("(0xA9) should set register A to nn, change N and Z flags", () => {
         Rom.data = new Uint8Array([0xA9, 0xFF, 0]);
         Flag.Z = 1;
-        
+
         Opcode[0xA9]();
         chai.assert.strictEqual(Register.A, Rom.data[1]);
         chai.assert.strictEqual(Flag.N, 1);
         chai.assert.strictEqual(Flag.Z, 0);
-        
+
         Opcode[0xA9]();
         chai.assert.strictEqual(Register.A, Rom.data[2]);
         chai.assert.strictEqual(Flag.N, 0);
         chai.assert.strictEqual(Flag.Z, 1);
     });
-    
+
     it("(0xAD) should set register A to nnnn, change N and Z flags", () => {
         RAM.set(0x01, 5);
         Rom.data = new Uint8Array([0xAD, 0x01, 0x00]);
-        
+
         Opcode[0xAD]();
         chai.assert.strictEqual(RAM.get(0x01), Register.A);
     });
@@ -184,61 +197,61 @@ describe("CPU Arithmetic/Logical Operations", () => {
         Register.Y = 0;
         Rom.data = new Uint8Array([]);
     });
-    
+
     it("(0x88) should decrement register Y by one, change N and Z flags", () => {
         let value: number = new Uint8Array([0xFF])[0];
         Register.Y = value;
         Flag.Z = 1;
-        
+
         Opcode[0x88]();
         chai.assert.strictEqual(Register.Y, value - 1);
         chai.assert.strictEqual(Flag.N, 1);
         chai.assert.strictEqual(Flag.Z, 0);
-        
+
         value = new Uint8Array([0x01])[0];
-        Register.Y = value;        
-        
+        Register.Y = value;
+
         Opcode[0x88]();
         chai.assert.strictEqual(Register.Y, value - 1);
         chai.assert.strictEqual(Flag.N, 0);
         chai.assert.strictEqual(Flag.Z, 1);
     });
-    
+
     it("(0xCA) should decrement register X by one, change N and Z flags", () => {
         let value: number = new Uint8Array([0xFF])[0];
         Register.X = value;
         Flag.Z = 1;
-        
+
         Opcode[0xCA]();
         chai.assert.strictEqual(Register.X, value - 1);
         chai.assert.strictEqual(Flag.N, 1);
         chai.assert.strictEqual(Flag.Z, 0);
-        
+
         value = new Uint8Array([0x01])[0];
-        Register.X = value;        
-        
+        Register.X = value;
+
         Opcode[0xCA]();
         chai.assert.strictEqual(Register.X, value - 1);
         chai.assert.strictEqual(Flag.N, 0);
         chai.assert.strictEqual(Flag.Z, 1);
     });
-    
+
     it("(0xE8) should increment register X by one, change N and Z flags", () => {
         let value: number = new Uint8Array([0xE8])[0];
         Register.X = value;
         Flag.Z = 1;
-        
+
         Opcode[0xE8]();
         chai.assert.strictEqual(Register.X, Convert.toUint8(value + 1));
         chai.assert.strictEqual(Flag.N, 1);
         chai.assert.strictEqual(Flag.Z, 0);
-        
+
         value = new Uint8Array([0xFF])[0];
-        Register.X = value;        
-        
+        Register.X = value;
+
         Opcode[0xE8]();
         chai.assert.strictEqual(Register.X, Convert.toUint8(value + 1));
         chai.assert.strictEqual(Flag.N, 0);
         chai.assert.strictEqual(Flag.Z, 1);
-    });  
+    });
 });
