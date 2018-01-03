c7982bf8f9a3952ab5982b89adf13f71d3a99dfd
Pavel Bakharev <baharevpavel@gmail.com>
2018-01-03 15:53:27 +0700

    BCD conversions, test for Convert

--- dev/Common.ts
+++ dev/Common.ts
@@ -2,21 +2,29 @@ export class Convert {
     public static toInt8(val: number) {
         return new Int8Array([val])[0];
     };
-    
+
     public static toUint8(val: number) {
-        return new Uint8Array([val])[0];
+        return val & 0xFF;
     };
-    
+
+    public static toBCD(val: number) {
+        return (val / 10 << 4) | val % 10;
+    };
+
+    public static toDec(val: number) {
+        return ((val >> 4) * 10) + (val & 0x0F);
+    };
+
     public static toBin(val: number) {
         let bits: string = (val >>> 0).toString(2);
-        
+
         if(bits.length > 8) {
             return ('0000000000000000' + bits).slice(-16);
         };
-        
+
         return ('00000000' + bits).slice(-8);
     };
-    
+
     public static toColorArray(hex: string) {
         let c: any = [];
         hex = hex.replace('#','');
@@ -24,5 +32,5 @@ export class Convert {
         c[1] = parseInt(hex.substring(2, 4), 16);
         c[2] = parseInt(hex.substring(4, 6), 16);
         return c;
-    };    
+    };
 };
---
+++ test/Common.spec.ts
@@ -0,0 +1,36 @@
+import {} from 'mocha';
+import {} from 'chai';
+
+import { Convert } from '../dev/Common';
+
+describe("Conversion functions", () => {
+    it("should convert number to Uint8", () => {
+        chai.assert.strictEqual(Convert.toUint8(257), 1);
+        chai.assert.strictEqual(Convert.toUint8(455), 199);
+    });
+
+    it("should convert number to Int8", () => {
+        chai.assert.strictEqual(Convert.toInt8(55), 55);
+        chai.assert.strictEqual(Convert.toInt8(240), -16);
+    });
+
+    it("should convert number to it's binary representation", () => {
+        chai.assert.strictEqual(Convert.toBin(3), '00000011');
+        chai.assert.strictEqual(Convert.toBin(24), '00011000');
+    });
+
+    it("should convert hex color to color array", () => {
+        chai.assert.strictEqual(Convert.toColorArray('#FF00FF').join(','), [255, 0, 255].join(','));
+        chai.assert.strictEqual(Convert.toColorArray('#FADD0F').join(','), [250, 221, 15].join(','));
+    });
+
+    it("should convert decimal to BCD", () => {
+        chai.assert.strictEqual(Convert.toBCD(37), 55);
+        chai.assert.strictEqual(Convert.toBCD(29), 41);
+    });
+
+    it("should convert BCD to decimal", () => {
+        chai.assert.strictEqual(Convert.toDec(55), 37);
+        chai.assert.strictEqual(Convert.toDec(41), 29);
+    });
+});
