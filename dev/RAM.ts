e59605ef11b71b1e3191e005763499c428477528
Pavel Bakharev <baharevpavel@gmail.com>
2018-04-02 11:09:02 +0700

    Added CXCLR write handler to RAM, current memory value is now being returned when reading from 'RAM write' event handler

--- dev/RAM.ts
+++ dev/RAM.ts
@@ -92,7 +92,7 @@ export class RAM {
 
 	// VSYNC write
 	private static 0x00(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x00];
 		if(!(RAM.get(0x00) & 0x02) && (value & 0x02)) {
 			TIA.scanline = 0;
 		};
@@ -101,14 +101,14 @@ export class RAM {
 
 	// WSYNC write
 	private static 0x02(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x02];
 		CPU.lock();
 		return value;
 	};
 
 	// NUSIZ0 write
 	private static 0x04(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x04];
 		let nusiz: Array<string> = Convert.toBin(value).split('');
 		let playerValue: number = parseInt(nusiz[5] + nusiz[6] + nusiz[7], 2);
 
@@ -120,7 +120,7 @@ export class RAM {
 
 	// NUSIZ1 write
 	private static 0x05(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x05];
 		let nusiz: Array<string> = Convert.toBin(value).split('');
 		let playerValue: number = parseInt(nusiz[5] + nusiz[6] + nusiz[7], 2);
 
@@ -132,35 +132,35 @@ export class RAM {
 
 	// COLUP0 write
 	private static 0x06(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x06];
 		TIA.pf.colup0 = TIA.p0.colup = TIA.m0.colup = TIA.colorPalette.get(value & 0xFE);
 		return value;
 	};
 
 	// COLUP1 write
 	private static 0x07(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x07];
 		TIA.pf.colup1 = TIA.p1.colup = TIA.m1.colup = TIA.colorPalette.get(value & 0xFE);
 		return value;
 	};
 
 	// COLUPF write
 	private static 0x08(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x08];
 		TIA.pf.colupf = TIA.ball.colupf = TIA.colorPalette.get(value & 0xFE);
 		return value;
 	};
 
 	// COLUBK write
 	private static 0x09(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x09];
 		TIA.bk.colubk = TIA.colorPalette.get(value & 0xFE);
 		return value;
 	};
 
 	// CTRLPF write
 	private static 0x0A(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x0A];
 		let ctrlpf: Array<string> = Convert.toBin(value).split('');
 
 		TIA.pf.ctrlpf = ctrlpf;
@@ -173,77 +173,77 @@ export class RAM {
 
 	// REFP0 write
 	private static 0x0B(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x0B];
 		TIA.p0.refp = ((value & 0x08) > 0);
 		return value;
 	};
 
 	// REFP1 write
 	private static 0x0C(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x0C];
 		TIA.p1.refp = ((value & 0x08) > 0);
 		return value;
 	};
 
 	// PF0 write
 	private static 0x0D(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x0D];
 		TIA.pf.pf0 = Convert.toBin(value).split('').reverse();
 		return value;
 	};
 
 	// PF1 write
 	private static 0x0E(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x0E];
 		TIA.pf.pf1 = Convert.toBin(value).split('').reverse();
 		return value;
 	};
 
 	// PF2 write
 	private static 0x0F(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x0F];
 		TIA.pf.pf2 = Convert.toBin(value).split('').reverse();
 		return value;
 	};
 
 	// RESP0 write
 	private static 0x10(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x10];
 		TIA.p0.position = (TIA.clock <= 68 ? 3 : TIA.clock - 61);
 		return value;
 	};
 
 	// RESP1 write
 	private static 0x11(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x11];
 		TIA.p1.position = (TIA.clock <= 68 ? 3 : TIA.clock - 61);
 		return value;
 	};
 
 	// RESM0 write
 	private static 0x12(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x12];
 		TIA.m0.position = (TIA.clock <= 68 ? 2 : TIA.clock - 68);
 		return value;
 	};
 
 	// RESM1 write
 	private static 0x13(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x13];
 		TIA.m1.position = (TIA.clock <= 68 ? 2 : TIA.clock - 68);
 		return value;
 	};
 
 	// RESBL write
 	private static 0x14(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x14];
 		TIA.ball.position = (TIA.clock <= 68 ? 2 : TIA.clock - 68);
 		return value;
 	};
 
 	// GRP0 write
 	private static 0x1B(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x1B];
 		TIA.p1.prevGrp = TIA.p1.grp;
 		TIA.p0.grp = Convert.toBin(value).split('');
 		return value;
@@ -251,7 +251,7 @@ export class RAM {
 
 	// GRP1 write
 	private static 0x1C(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x1C];
 		TIA.p0.prevGrp = TIA.p0.grp;
 		TIA.ball.prevEnabl = TIA.ball.enabl;
 		TIA.p1.grp = Convert.toBin(value).split('');
@@ -260,28 +260,28 @@ export class RAM {
 
 	// ENAM0 write
 	private static 0x1D(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x1D];
 		TIA.m0.enam = ((value & 0x02) > 0);
 		return value;
 	};
 
 	// ENAM1 write
 	private static 0x1E(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x1E];
 		TIA.m1.enam = ((value & 0x02) > 0);
 		return value;
 	};
 
 	// ENABL write
 	private static 0x1F(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x1F];
 		TIA.ball.enabl = ((value & 0x02) > 0);
 		return value;
 	};
 
 	// HMP0 write
 	private static 0x20(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x20];
 		let bit: Array<string> = Convert.toBin(value).split('');
 		TIA.p0.hmp = parseInt(bit[1] + bit[2] + bit[3], 2) - (bit[0] == '1' ? 8 : 0);
 		return value;
@@ -289,7 +289,7 @@ export class RAM {
 
 	// HMP1 write
 	private static 0x21(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x21];
 		let bit: Array<string> = Convert.toBin(value).split('');
 		TIA.p1.hmp = parseInt(bit[1] + bit[2] + bit[3], 2) - (bit[0] == '1' ? 8 : 0);
 		return value;
@@ -297,7 +297,7 @@ export class RAM {
 
 	// HMM0 write
 	private static 0x22(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x22];
 		let bit: Array<string> = Convert.toBin(value).split('');
 		TIA.m0.hmm = parseInt(bit[1] + bit[2] + bit[3], 2) - (bit[0] == '1' ? 8 : 0);
 		return value;
@@ -305,7 +305,7 @@ export class RAM {
 
 	// HMM1 write
 	private static 0x23(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x23];
 		let bit: Array<string> = Convert.toBin(value).split('');
 		TIA.m1.hmm = parseInt(bit[1] + bit[2] + bit[3], 2) - (bit[0] == '1' ? 8 : 0);
 		return value;
@@ -313,7 +313,7 @@ export class RAM {
 
 	// HMBL write
 	private static 0x24(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x24];
 		let bit: Array<string> = Convert.toBin(value).split('');
 		TIA.ball.hmbl = parseInt(bit[1] + bit[2] + bit[3], 2) - (bit[0] == '1' ? 8 : 0);
 		return value;
@@ -321,28 +321,28 @@ export class RAM {
 
 	// VDELP0 write
 	private static 0x25(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x25];
 		TIA.p0.vdelp = ((value & 0x01) > 0);
 		return value;
 	};
 
 	// VDELP1 write
 	private static 0x26(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x26];
 		TIA.p1.vdelp = ((value & 0x01) > 0);
 		return value;
 	};
 
 	// VDELBL write
 	private static 0x27(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x27];
 		TIA.ball.vdelbl = ((value & 0x01) > 0);
 		return value;
 	};
 
 	// HMOVE write
 	private static 0x2A(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x2A];
 
 		TIA.p0.position = (TIA.p0.position - TIA.p0.hmp) % 160;
 		TIA.p1.position = (TIA.p1.position - TIA.p1.hmp) % 160;
@@ -355,9 +355,16 @@ export class RAM {
 
 	// HMCLR write
 	private static 0x2B(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x2B];
 		this.memory[0x20] = this.memory[0x21] = this.memory[0x22] = this.memory[0x23] = this.memory[0x24] = 0;
 		TIA.p0.hmp = TIA.p1.hmp = TIA.m0.hmm = TIA.m1.hmm = TIA.ball.hmbl = 0;
+		return value;
+	};
+	
+	// CXCLR write
+	private static 0x2C(value: number) {
+		if(value === undefined) return this.memory[0x2C];
+		this.memory[0x30] = this.memory[0x31] = this.memory[0x32] = this.memory[0x33] = this.memory[0x34] = this.memory[0x35] = this.memory[0x36] = this.memory[0x37] = 0;
 		return value;
 	};
 
@@ -401,6 +408,7 @@ export class RAM {
 		return parseInt(result.join(''), 2);
 	};
 
+	// INTIM read
 	private static 0x284() {
 		if(!PIA.timer) {
 			PIA.timer = PIA.prevTimer || 0x296;
@@ -420,7 +428,7 @@ export class RAM {
 
 	// TIM1T write
 	private static 0x294(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x294];
 		let bits: Array<string> = Convert.toBin(this.memory[0x285]).split('');
 
 		value = new Uint8Array([value - 1])[0];
@@ -433,7 +441,7 @@ export class RAM {
 
 	// TIM8T write
 	private static 0x295(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x295];
 		let bits: Array<string> = Convert.toBin(this.memory[0x285]).split('');
 
 		value = new Uint8Array([value - 1])[0];
@@ -446,7 +454,7 @@ export class RAM {
 
 	// TIM64T write
 	private static 0x296(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x296];
 		let bits: Array<string> = Convert.toBin(this.memory[0x285]).split('');
 
 		value = new Uint8Array([value - 1])[0];
@@ -459,7 +467,7 @@ export class RAM {
 
 	// TIM1024T write
 	private static 0x297(value: number) {
-		if(value === undefined) return;
+		if(value === undefined) return this.memory[0x297];
 		let bits: Array<string> = Convert.toBin(this.memory[0x285]).split('');
 
 		value = new Uint8Array([value - 1])[0];
